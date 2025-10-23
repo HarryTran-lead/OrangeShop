b9822ac963e87f6b7789a8194f8d4bba6e2f7245
Harry <thcstrandainghiathinh6a1@gmail.com>
2025-10-23 13:39:32 +0700

    turn off roledown

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
+// vite.config.ts (hoặc .js)
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  experimental: {
+    enableNativePlugin: false, // 🔧 tránh lỗi rolldown native binding trên CI
+  },
 })
